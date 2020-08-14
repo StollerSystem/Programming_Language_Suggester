93cf27a5a6a2fc899a91274a60ddaed3fe4a1203
Ben Stoller <stollersystemsound@gmail.com>
2020-08-14 09:14:13 -0700

    add initial basic css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 // Business Logic
 
+function answerBranch(answer1,answer2,answer3,answer4,answer5) {
+
+  if (answer5 === "hack"){
+    $("#mainForm").hide();
+    $("#mainTop").hide();
+  } 
+
+
+
+}
+
 
 
 
@@ -12,12 +23,11 @@ $(document).ready(function() {
     event.preventDefault(event);
     let answer1 = $("#question1").val();
     let answer2 = $("#question2").val();
-    let answer3 = $("input:radio[name=question3]:checked").val()
-    let answer4 = $("input:radio[name=question4]:checked").val()
-    let answer5 = $("#question5").val()
-
-    console.log(answer1,answer2,answer3,answer4,answer5)
-
+    let answer3 = $("input:radio[name=question3]:checked").val();
+    let answer4 = $("input:radio[name=question4]:checked").val();
+    let answer5 = $("#question5").val().toLowerCase();
+    //console.log(answer1,answer2,answer3,answer4,answer5);
+    answerBranch(answer1,answer2,answer3,answer4,answer5);
   });
 
 });
