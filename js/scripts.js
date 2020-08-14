487014e5f4e6f8cb473cf68676fc202c0e69c966
Ben Stoller <stollersystemsound@gmail.com>
2020-08-14 09:51:12 -0700

    start working on survey questions

--- js/scripts.js
+++ js/scripts.js
@@ -21,12 +21,13 @@ $(document).ready(function() {
 
   $("#mainForm").submit(function(event) {
     event.preventDefault(event);
+    let name = $("#name").val();
     let answer1 = $("#question1").val();
     let answer2 = $("#question2").val();
     let answer3 = $("input:radio[name=question3]:checked").val();
     let answer4 = $("input:radio[name=question4]:checked").val();
     let answer5 = $("#question5").val().toLowerCase();
-    //console.log(answer1,answer2,answer3,answer4,answer5);
+    console.log(name,answer1,answer2,answer3,answer4,answer5);
     answerBranch(answer1,answer2,answer3,answer4,answer5);
   });
 
